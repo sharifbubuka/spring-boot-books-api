be2acc34c2c9a46825f7c97983d738846ffed5e0
Bubuka Sharif <sharifbubuka256@gmail.com>
2023-10-23 00:08:15 +0200

    initial commit

---
+++ src/main/java/com/bubukasharif/springbootbooksapi/SpringBootBooksApiApplication.java
@@ -0,0 +1,13 @@
+package com.bubukasharif.springbootbooksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBooksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bubukasharif/springbootbooksapi/SpringBootBooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bubukasharif.springbootbooksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
